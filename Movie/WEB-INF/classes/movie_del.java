0becfaffb11b2f9911d74e65e48a49366123a015
JCSI2022 <113455662+LF92R@users.noreply.github.com>
2024-03-30 13:15:27 -0400

    Update movie_del.java
    Now accommodates for when the movie table is empty, if the entry does not exist, and if the entry does exist it deletes it.

--- Movie/WEB-INF/classes/movie_del.java
+++ Movie/WEB-INF/classes/movie_del.java
@@ -19,16 +19,16 @@ public class movie_del extends HttpServlet
 		try
 		{			
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver()); 
-            con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "project", "project");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "C##project", "project");
 	       	System.out.println("Congratulations! You are connected successfully.");      
      	}
         catch(SQLException e)
 		{	
-			System.out.println("Error: "+e);	
+			System.out.println("Error: " + e);	
 		}
 		catch(Exception e) 
 		{
-			System.err.println("Exception while loading  driver");		
+			System.err.println("Exception while loading driver");		
 		}
 	    try 
 		{
@@ -40,45 +40,72 @@ public class movie_del extends HttpServlet
 		}
 		
 		response.setContentType("text/html");
-		PrintWriter out = null ;
+		PrintWriter out = null;
+
 		try
 		{
-			out =  response.getWriter();
+			out = response.getWriter();
 		}
 		catch (IOException e) 
 		{
   			e.printStackTrace();
 		}
 		
-		query = "delete  from movie where m_id = '"+m_id+"' or m_title = '"+m_title+"'";
+		query = "delete from movie where m_id = '"+m_id+"' or m_title = '"+m_title+"'";
 		
 		out.println("<html><head><title>Movie has been deleted</title>");	 
 		out.println("</head><body>");
-		
-		out.print( "<br /><b><center><font color=\"BLACK\"><H2>One Record has deleted</H2></font>");
-        out.println( "</center><br />" );
+
        	try 
 		{ 
-			result=state4.executeQuery(query);
-				
+			//For case where the movie table is empty.
+			ResultSet countResult = state4.executeQuery("select count(*) from movie");
+			countResult.next();
+			int rowCount = countResult.getInt(1);
+
+			//If the movie table is empty, state that it is so. If not, state the movie entry has been deleted from the movie table.
+			if (rowCount == 0) {
+				out.print( "<br /><b><center><font color=\"BLACK\"><H2>The movie table is empty</H2></font>");
+				out.println( "</center><br />" );
+			} else {
+				ResultSet test = state4.executeQuery("select m_id from movie where m_id = '" + m_id + "'");
+				if (test.next()) {
+					// Movie with given m_id exists
+					if (test.getInt(1) == Integer.parseInt(m_id)) {
+						out.print("<br /><b><center><font color=\"BLACK\"><H2>The movie entry has been deleted.</H2></font>");
+						out.println("</center><br />");
+					}
+				} else {
+					// Movie with given m_id does not exist
+					out.print("<br /><b><center><font color=\"BLACK\"><H2>There is no such entry</H2></font>");
+					out.println("</center><br />");
+				}
+			}	
+
+			//For executing the query to delete the movie entry.
+			result = state4.executeQuery(query);
+
 	  	}
 		catch (SQLException e) 
 		{
+			//If the query fails to execute, catch exception.
 			System.err.println("SQLException while executing SQL Statement."); 
 		}
+
 		out.println("<center><table border=\"1\">"); 
 		out.println("<tr BGCOLOR=\"#cccccc\">");
-          out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\"> </td>");
-       // out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">movtitle</td>");
+        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\"> </td>");
+       	// out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">movtitle</td>");
         out.println("</tr>");
+
 		try 
 		{ 
             while(result.next()) 
 			{ 
 		    		out.println("<tr>");
-                		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(1)+"</td>");
+                	out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">" + result.getString(1) + "</td>");
 		    		//out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(2)+"</td>");
-                		out.println("</tr>");
+                	out.println("</tr>");
 			} 
 	    }
 		catch (SQLException e) 
@@ -87,6 +114,7 @@ public class movie_del extends HttpServlet
 		}
 
 		out.println("</table></CENTER>");
+
 		try 
 		{ 
    			result.close(); 
