d92f0f8f4e23ab8080df2f6ced42d6cdfcbad9d1
JCSI2022 <113455662+LF92R@users.noreply.github.com>
2024-04-01 12:43:09 -0400

    Update person_reg.java
    Incomplete, but can successfully add to a person to the person table its respective role table should there be no errors thrown. Still needs to move some code to a separate method. Will update this again later.

--- Movie/WEB-INF/classes/person_reg.java
+++ Movie/WEB-INF/classes/person_reg.java
@@ -4,94 +4,109 @@ import java.io.*;
 import java.sql.*;
 
 public class person_reg extends HttpServlet {
-  // Use a prepared statement to store a student into the database
-  private PreparedStatement pstmt;
+    private PreparedStatement pstmt;
+    private Connection conn;
 
-  /** Initialize global variables */
-  public void init() throws ServletException {
-    initializeJdbc(); // method, go to line 53
-  }
-
-  /** Process the HTTP Post request */
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-
-    // Obtain parameters from the client
-    String m_id = request.getParameter("m_id");
-    String m_title = request.getParameter("m_title");
-    String m_date = request.getParameter("m_date");
-    String m_synopsis = request.getParameter("m_synopsis");
-    String m_length = request.getParameter("m_length");
-    String rating_id = request.getParameter("rating_id");
-    String cat_id = request.getParameter("cat_id");
-
-    try {
-
-      // out.println("First Name: " + firstName );
-      if (m_id.length() == 0 || m_title.length() == 0) {
-        out.println("Please: Moive ID and title are required");
-        return; // End the method
-      }
-
-      storeStudent(m_id, m_title, m_date, m_synopsis, m_length, rating_id,
-          cat_id);
-
-      out.println(m_id + " " + m_title +
-          " is now registered in the database");
-    } catch (Exception ex) {
-      out.println("\n Error: " + ex.getMessage());
-    } finally {
-      out.close(); // Close stream
+    public void init() throws ServletException {
+        initializeJdbc();
     }
-  }
 
-  /** Initialize database connection */
-  private void initializeJdbc() {
-    try {
-      // Declare driver and connection string
-      // String driver = "sun.jdbc.odbc.JdbcOdbcDriver";
-      // String connectionString = "jdbc:odbc:exampleMDBDataSource";
-      // For MySQL
-      // String driver = "com.mysql.jdbc.Driver";
-      // String connectionString = "jdbc:mysql://localhost/test";
-      // For Oracle
-      String driver = "oracle.jdbc.driver.OracleDriver";
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-      // Load the driver
-      Class.forName(driver);
+        String p_id = request.getParameter("p_id");
+        String p_lastName = request.getParameter("p_lastName");
+        String p_firstName = request.getParameter("p_firstName");
+        String p_pay = request.getParameter("p_pay");
+        String role_name = request.getParameter("role_name");
+        String role_id = request.getParameter("role_id");
+        String position = request.getParameter("position");
+        String m_id = request.getParameter("m_id");
 
-      String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl"; // the oracle driver's url - the source locator. port 1521.
-                                                            // orcl is the default database name, so you will need to
-                                                            // change that if you named it differently.
-      String user = "project"; // this is the username for your database user
-      String password = "project"; // this is the password for your database user
+        try {
+            if (p_id == null || p_id.isEmpty()) {
+                out.println("Please add an entry for all required values.");
+                return;
+            }
 
-      // Connect to the sample database
-      Connection conn = DriverManager.getConnection // the method getConnection() is defined within sql.
-      (url, user, password);
+            storePerson(p_id, p_lastName, p_firstName, p_pay);
+            out.println(p_id + ": " + p_firstName + " " + p_lastName + " is now registered in the database, check if relevant role table has an entry");
 
-      // Create a Statement - this is where the sql code is
-      pstmt = conn.prepareStatement("insert into movie " +
-          "(m_id, m_title, m_date, m_synopsis, m_length, rating_id, "
-          + "cat_id) values (?, ?, ?, ?, ?, ?, ?)");
-    } catch (Exception ex) {
-      ex.printStackTrace();
+            // Construct SQL statement for role insertion based on role_id
+            String roleAttributeName_ID = "";
+            String roleAttributeName_Position = "";
+            switch(role_name) {
+                case "Producer":
+                    roleAttributeName_ID = "prod_ID";
+                    roleAttributeName_Position = "prod_position";
+                    break;
+                case "Actor":
+                    roleAttributeName_ID = "actor_ID";
+                    roleAttributeName_Position = "actor_role";
+                    break;
+                case "Actress":
+                    roleAttributeName_ID = "actress_ID";
+                    roleAttributeName_Position = "actress_role";
+                    break;
+                case "Writer":
+                    roleAttributeName_ID = "writ_ID";
+                    roleAttributeName_Position = "writ_contr";
+                    break;
+                case "Director":
+                    roleAttributeName_ID = "dir_ID";
+                    roleAttributeName_Position = "dir_position";
+                    break;
+                default:
+                    out.println("\n Error in passing attribute role_name.");
+            }
+
+            // Insert into role table - MOVE THIS TO A METHOD storeRole() LATER!!!!
+            String roleInsertSQL = "insert into " + role_name + " (" + roleAttributeName_ID + ", " + roleAttributeName_Position + ", p_id, m_id) VALUES (?, ?, ?, ?)";
+            PreparedStatement roleStatement = conn.prepareStatement(roleInsertSQL);
+            roleStatement.setString(1, role_id); 
+            roleStatement.setString(2, position);
+            roleStatement.setString(3, p_id);
+            roleStatement.setString(4, m_id);
+            roleStatement.executeUpdate();
+            //roleStatement.close();
+            
+            /*COME BACK - need to have an if statement to check if the person and role they're in have their entries created.
+              If not, delete the extra person.*/
+
+        } catch (Exception ex) {
+            out.println("\n Error: " + ex.getMessage());
+        } finally {
+            out.close();
+        }
     }
-  }
 
-  /** Store a student record to the database */
-  private void storeStudent(String m_id, String m_title,
-      String m_date, String m_synopsis, String m_length, String rating_id,
-      String cat_id) throws SQLException {
-    pstmt.setString(1, m_id);
-    pstmt.setString(2, m_title);
-    pstmt.setString(3, m_date);
-    pstmt.setString(4, m_synopsis);
-    pstmt.setString(5, m_length);
-    pstmt.setString(6, rating_id);
-    pstmt.setString(7, cat_id);
+    private void initializeJdbc() {
+        try {
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            Class.forName(driver);
 
-    pstmt.executeUpdate();
-  }
+            String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+            String user = "C##CSI3450";
+            String password = "johnRing19841";
+
+            conn = DriverManager.getConnection(url, user, password);
+
+            pstmt = conn.prepareStatement("INSERT INTO person (p_id, p_lastName, p_firstName, p_pay) VALUES (?, ?, ?, ?)");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void storePerson(String p_id, String p_lastName, String p_firstName, String p_pay) throws SQLException {
+        pstmt.setString(1, p_id);
+        pstmt.setString(2, p_lastName);
+        pstmt.setString(3, p_firstName);
+        pstmt.setString(4, p_pay);
+        pstmt.executeUpdate();
+        //pstmt.close();
+    }
+
+    
 }
