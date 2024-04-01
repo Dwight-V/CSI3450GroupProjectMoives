8b8c14c13a29f96503b67d9d65d4f14d767712ed
ATJ <145053177+ATJOakland@users.noreply.github.com>
2024-03-31 20:49:00 -0400

    Add files via upload

---
+++ Documentation - READ BEFORE RUNNING/CourseProjectTables.sql
@@ -0,0 +1,91 @@
+CREATE TABLE movie (
+    m_ID INT,
+    m_title VARCHAR(100),
+    m_releaseDate DATE,
+    m_synopsis VARCHAR(300),
+    m_rating VARCHAR(10),
+    m_length INT,
+    cat_id VARCHAR(50), --horror, action, comedy, etc.
+    productionStatus VARCHAR(50), --ADDED. Approved for filming, screened in theatres, coming soon, debut, released on Blu-Ray, Amazon Video, Netflix, etc.
+    PRIMARY KEY (m_ID)
+);
+
+CREATE TABLE person (
+    p_ID INT,
+    p_lastName VARCHAR(50),
+    p_firstName VARCHAR(50),
+    p_pay DECIMAL(15, 2),
+    PRIMARY KEY(p_ID)
+);
+
+CREATE TABLE theatre (
+    t_ID INT,
+    t_name VARCHAR(50),
+    t_street VARCHAR(50),
+    t_city VARCHAR(50),
+    t_state VARCHAR(50),
+    t_zip INT, 
+    t_country VARCHAR(50),
+    PRIMARY KEY (t_ID)
+);
+
+CREATE TABLE producer (
+    prod_ID INT,
+    prod_position VARCHAR(50), 
+    p_ID INT,
+    m_ID INT,
+    PRIMARY KEY (prod_ID, m_ID),
+    CONSTRAINT fk_has_p_ID_producer FOREIGN KEY(p_ID) REFERENCES person(p_ID),
+    CONSTRAINT fk_has_m_ID_producer FOREIGN KEY (m_ID) REFERENCES movie(m_ID)
+);
+
+CREATE TABLE actor (
+    actor_ID INT,
+    actor_role VARCHAR(50),
+    p_ID INT,
+    m_ID INT,
+    PRIMARY KEY (actor_ID, m_ID),
+    CONSTRAINT fk_has_p_ID_actor FOREIGN KEY (p_ID) REFERENCES person(p_ID),
+    CONSTRAINT fk_has_m_ID_actor FOREIGN KEY (m_ID) REFERENCES movie(m_ID)
+);
+
+CREATE TABLE actress (
+    actress_ID INT,
+    actress_role VARCHAR(50),
+    p_ID INT,
+    m_ID INT,
+    PRIMARY KEY (actress_ID, m_ID),
+    CONSTRAINT fk_has_p_ID_actress FOREIGN KEY(p_ID) REFERENCES person(p_ID),
+    CONSTRAINT fk_has_m_ID_actress FOREIGN KEY (m_ID) REFERENCES movie(m_ID)
+);
+
+CREATE TABLE writer (
+    writ_ID INT,
+    writ_contr VARCHAR(100),
+    p_ID INT,
+    m_ID INT,
+    PRIMARY KEY (writ_ID, m_ID),
+    CONSTRAINT fk_has_p_ID_writer FOREIGN KEY(p_ID) REFERENCES person(p_ID),
+    CONSTRAINT fk_has_m_ID_writer FOREIGN KEY (m_ID) REFERENCES movie(m_ID)
+);
+
+CREATE TABLE director (
+    dir_ID INT,
+    dir_position VARCHAR(50),
+    p_ID INT,
+    m_ID INT,
+    PRIMARY KEY (dir_ID, m_ID),
+    CONSTRAINT fk_has_personID_director FOREIGN KEY(p_ID) REFERENCES person(p_ID),
+    CONSTRAINT fk_has_movieID_director FOREIGN KEY (m_ID) REFERENCES movie(m_ID)
+);
+
+CREATE TABLE theatremovie (
+    t_m_ID INT,
+    t_m_start TIMESTAMP,
+    t_m_end TIMESTAMP,
+    m_ID INT,
+    t_ID INT,
+    PRIMARY KEY (t_m_ID, m_ID, t_ID),
+    CONSTRAINT fk_has_m_ID_theatremovie FOREIGN KEY (m_ID) REFERENCES movie(m_ID),
+    CONSTRAINT fk_has_t_ID_theatremovie FOREIGN KEY (t_ID) REFERENCES theatre(t_ID)
+);
--- Movie/WEB-INF/classes/movie_upd.java
+++ Movie/WEB-INF/classes/movie_upd.java
@@ -13,30 +13,28 @@ public class movie_upd extends HttpServlet
 			String query="";        
 			Connection con=null; 
           
-			String m_id = request.getParameter("m_id");
+            String m_id = request.getParameter("m_id");
 			String m_title = request.getParameter("m_title");
-			String m_releaseDate = request.getParameter("m_releaseDate");
+			String m_date = request.getParameter("m_date");
 			String m_synopsis = request.getParameter("m_synopsis");
 			String m_length = request.getParameter("m_length");
-			String m_rating = request.getParameter("m_rating");
+			String rating_id = request.getParameter("rating_id");
 			String cat_id = request.getParameter("cat_id");
-			String productionStatus = request.getParameter("productionStatus");
 
 		try
 		{			
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver()); 
-            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "C##project", "project");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "project", "project");
 	       	System.out.println("Congratulations! You are connected successfully.");      
      	}
         catch(SQLException e)
 		{	
-			System.out.println("Error: "+ e);	
+			System.out.println("Error: "+e);	
 		}
 		catch(Exception e) 
 		{
 			System.err.println("Exception while loading  driver");		
 		}
-
 	    try 
 		{
         	state4 = con.createStatement();
@@ -48,7 +46,6 @@ public class movie_upd extends HttpServlet
 		
 		response.setContentType("text/html");
 		PrintWriter out = null ;
-
 		try
 		{
 			out =  response.getWriter();
@@ -58,52 +55,27 @@ public class movie_upd extends HttpServlet
   			e.printStackTrace();
 		}
 		
-		query = "update movie set m_title = '"+m_title+"', m_releaseDate = DATE '"+m_releaseDate+"', m_synopsis = '"+m_synopsis+"', m_length = '"+m_length+"', m_rating = '"+m_rating+"', cat_id = '"+cat_id+"', productionStatus = '"+productionStatus+"' where m_id = '"+m_id+"'";
-
-	
-		out.println("<html><head><title>Movie has been updated</title>");	 
+		query = "update  movie set m_title = '"+m_title+"', m_date = '"+m_date+"', m_length = '"+m_length+"', rating_id = '"+rating_id+"', cat_id = '"+cat_id+"', m_synopsis = '"+m_synopsis+"'	where m_id = '"+m_id+"'";
+		
+		out.println("<html><head><title>Movie has been updtated</title>");	 
 		out.println("</head><body>");
-       	
-		try 
+		
+		out.print( "<br /><b><center><font color=\"BLACK\"><H2>One Record has updated</H2></font>");
+        out.println( "</center><br />" );
+       	try 
 		{ 
-			//For case where the movie table is empty.
-			ResultSet countResult = state4.executeQuery("select count(*) from movie");
-			countResult.next();
-			int rowCount = countResult.getInt(1);
-
-			if (rowCount == 0) {
-				//Movie table is empty. 
-				out.print( "<br /><b><center><font color=\"BLACK\"><H2>The movie table is empty</H2></font>");
-				out.println( "</center><br />" );
-			} else {
-				ResultSet test = state4.executeQuery("select m_id from movie where m_id = '" + m_id + "'");
-				if (test.next()) {
-					// Movie with given m_id exists
-					if (test.getInt(1) == Integer.parseInt(m_id)) {
-						out.print("<br /><b><center><font color=\"BLACK\"><H2>The movie entry has been updated.</H2></font>");
-						out.println("</center><br />");
-					}
-				} else {
-					// Movie with given m_id does not exist
-					out.print("<br /><b><center><font color=\"BLACK\"><H2>There is no such entry</H2></font>");
-					out.println("</center><br />");
-				}
-			}	
-
-			//For executing the query to update the movie entry.
-			result = state4.executeQuery(query);
+			result=state4.executeQuery(query);
+				
 	  	}
 		catch (SQLException e) 
 		{
 			System.err.println("SQLException while executing SQL Statement."); 
 		}
-
 		out.println("<center><table border=\"1\">"); 
 		out.println("<tr BGCOLOR=\"#cccccc\">");
           out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\"> </td>");
        // out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">movtitle</td>");
         out.println("</tr>");
-
 		try 
 		{ 
             while(result.next()) 
@@ -116,7 +88,7 @@ public class movie_upd extends HttpServlet
 	    }
 		catch (SQLException e) 
 		{
-			System.out.println("Resultset is not connected"); 
+			System.out.println("Resutset is not connected"); 
 		}
 
 		out.println("</table></CENTER>");
