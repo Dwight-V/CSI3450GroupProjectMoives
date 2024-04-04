c8e5e1798034fa33301e556efd0b258ea8e163d4
JCSI2022 <113455662+LF92R@users.noreply.github.com>
2024-04-04 11:48:18 -0400

    Update person_reg.java
    Adds a person entry, then a role table entry determined by the role_name value entered. If it fails to enter into the role table, the person entry is also deleted.

--- Movie/WEB-INF/classes/person_reg.java
+++ Movie/WEB-INF/classes/person_reg.java
@@ -4,8 +4,10 @@ import java.io.*;
 import java.sql.*;
 
 public class person_reg extends HttpServlet {
-    private PreparedStatement pstmt;
+    private PreparedStatement personPstmt, rolePstmt;
     private Connection conn;
+    private Statement extraPersonDelete;
+    private PrintWriter out;
 
     public void init() throws ServletException {
         initializeJdbc();
@@ -13,7 +15,7 @@ public class person_reg extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        out = response.getWriter();
 
         String p_id = request.getParameter("p_id");
         String p_lastName = request.getParameter("p_lastName");
@@ -31,8 +33,7 @@ public class person_reg extends HttpServlet {
             }
 
             storePerson(p_id, p_lastName, p_firstName, p_pay);
-            out.println(p_id + ": " + p_firstName + " " + p_lastName + " is now registered in the database, check if relevant role table has an entry");
-
+        
             // Construct SQL statement for role insertion based on role_id
             String roleAttributeName_ID = "";
             String roleAttributeName_Position = "";
@@ -61,18 +62,9 @@ public class person_reg extends HttpServlet {
                     out.println("\n Error in passing attribute role_name.");
             }
 
-            // Insert into role table - MOVE THIS TO A METHOD storeRole() LATER!!!!
-            String roleInsertSQL = "insert into " + role_name + " (" + roleAttributeName_ID + ", " + roleAttributeName_Position + ", p_id, m_id) VALUES (?, ?, ?, ?)";
-            PreparedStatement roleStatement = conn.prepareStatement(roleInsertSQL);
-            roleStatement.setString(1, role_id); 
-            roleStatement.setString(2, position);
-            roleStatement.setString(3, p_id);
-            roleStatement.setString(4, m_id);
-            roleStatement.executeUpdate();
-            //roleStatement.close();
-            
-            /*COME BACK - need to have an if statement to check if the person and role they're in have their entries created.
-              If not, delete the extra person.*/
+            storeRole(roleAttributeName_ID, roleAttributeName_Position, p_id, m_id, role_id, role_name, position);
+
+            out.println(p_id + ": " + p_firstName + " " + p_lastName + " is now registered in the database in tables Person and role table " + role_name + ".");
 
         } catch (Exception ex) {
             out.println("\n Error: " + ex.getMessage());
@@ -92,7 +84,7 @@ public class person_reg extends HttpServlet {
 
             conn = DriverManager.getConnection(url, user, password);
 
-            pstmt = conn.prepareStatement("INSERT INTO person (p_id, p_lastName, p_firstName, p_pay) VALUES (?, ?, ?, ?)");
+            personPstmt = conn.prepareStatement("INSERT INTO person (p_id, p_lastName, p_firstName, p_pay) VALUES (?, ?, ?, ?)");
 
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -100,13 +92,49 @@ public class person_reg extends HttpServlet {
     }
 
     private void storePerson(String p_id, String p_lastName, String p_firstName, String p_pay) throws SQLException {
-        pstmt.setString(1, p_id);
-        pstmt.setString(2, p_lastName);
-        pstmt.setString(3, p_firstName);
-        pstmt.setString(4, p_pay);
-        pstmt.executeUpdate();
-        //pstmt.close();
+        personPstmt.setString(1, p_id);
+        personPstmt.setString(2, p_lastName);
+        personPstmt.setString(3, p_firstName);
+        personPstmt.setString(4, p_pay);
+        personPstmt.executeUpdate();
+
     }
 
-    
+    private void storeRole(String roleAttributeName_ID, String roleAttributeName_Position, String p_id, String m_id, 
+    String role_id, String role_name, String position) throws SQLException {
+
+        try 
+        {
+           // Start a transaction
+            conn.setAutoCommit(false);
+
+            // Insert into role table determined by choice of role position.
+            String roleInsertSQL = "INSERT INTO " + role_name + " (" + roleAttributeName_ID + ", " + roleAttributeName_Position + ", p_ID, m_ID) VALUES (?, ?, ?, ?)";
+            rolePstmt = conn.prepareStatement(roleInsertSQL);
+            rolePstmt.setString(1, role_id); 
+            rolePstmt.setString(2, position);
+            rolePstmt.setString(3, p_id);
+            rolePstmt.setString(4, m_id);
+            int roleTableRowsAffected = rolePstmt.executeUpdate();
+
+            if (roleTableRowsAffected > 0) {
+                conn.commit();
+            }
+        }
+        catch (SQLException e)
+        {
+            conn.rollback();
+            // Delete the person entry
+            extraPersonDelete = conn.createStatement();
+            extraPersonDelete.executeUpdate("DELETE FROM person WHERE p_id = " + p_id);
+            conn.commit();
+
+            out.println("\n Error: " + e.getMessage() + ". " + role_name + " table addition has failed. " +
+                "\n Person entry with p_ID = " + p_id + " has been deleted.");
+        } finally {
+            // Set auto-commit to true to revert to default behavior
+            conn.setAutoCommit(true);
+        }
+
+    }
 }
