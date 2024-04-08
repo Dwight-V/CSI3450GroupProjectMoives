c9897edf195775ade9498a87d21e46c4a77fdafc
Dwight <112441507+Dwight-V@users.noreply.github.com>
2024-04-08 18:45:29 -0400

    TheatreMovie (#6)
    * Add files via upload
    
    * Add files via upload
    
    * Create TheatreMovie_reg.html
    
    Coded by jxkada (Joseph).
    
    * Add files via upload
    
    For adding to TheatreMovie. Coded almost all by jxkada. Some additions/edits made by me.
    
    * Update web.xml
    
    Now with TheatreMovie.
    
    * Update index.html
    
    Removed Director. Added TheatreMovie.
    
    * Update TheatreMovie_reg.java
    
    username and password are now C##project, project respectively
    
    * Add files via upload
    
    * TheatreMovieJdbc.java working
    
    * theatremovie_del.java working
    
    * Corrected TheatreMovie_del.html form call
    
    * TheatreMovie_upd should be working, but is infinitely loading on submit
    
    * TheatreMovie_upd.java working
    
    * Removed Documentation folder - this branch is for TheatreMovie only
    
    ---------
    
    Co-authored-by: ATJ <145053177+ATJOakland@users.noreply.github.com>
    Co-authored-by: JCSI2022 <113455662+LF92R@users.noreply.github.com>

--- Movie/WEB-INF/classes/movie_reg.java
+++ Movie/WEB-INF/classes/movie_reg.java
@@ -3,46 +3,48 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.sql.*;
 
-public class movie_reg extends HttpServlet {
+public class movie_reg extends HttpServlet { 
   // Use a prepared statement to store a student into the database
   private PreparedStatement pstmt;
 
   /** Initialize global variables */
-  public void init() throws ServletException {
-    initializeJdbc();
+  public void init() throws ServletException { 
+    initializeJdbc(); //method, go to line 53
   }
 
   /** Process the HTTP Post request */
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse
+      response) throws ServletException, IOException {
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
 
     // Obtain parameters from the client
-    String m_id = request.getParameter("m_id");
+    String m_id = request.getParameter("m_id"); 
     String m_title = request.getParameter("m_title");
-    String m_releaseDate = request.getParameter("m_releaseDate");
+    String m_date = request.getParameter("m_date");
     String m_synopsis = request.getParameter("m_synopsis");
     String m_length = request.getParameter("m_length");
-    String m_rating = request.getParameter("m_rating");
+    String rating_id = request.getParameter("rating_id");
     String cat_id = request.getParameter("cat_id");
-    String productionStatus = request.getParameter("productionStatus");
-
-    // Rid of the DATE at the start of the string
-    m_releaseDate = m_releaseDate.replaceAll("DATE", "");
 
     try {
 
+		//out.println("First Name: " + firstName );
       if (m_id.length() == 0 || m_title.length() == 0) {
-        out.println("Movie ID and title are required to add a movie.");
+        out.println("Please: Moive ID and title are required");
         return; // End the method
       }
 
-      storeMovie(m_id, m_title, m_releaseDate, m_synopsis, m_length, m_rating, cat_id, productionStatus);
+      storeStudent(m_id, m_title,m_date, m_synopsis, m_length, rating_id,
+        cat_id);
 
-      out.println(m_id + " " + m_title + " is now registered in the database");
-    } catch (Exception ex) {
+      out.println(m_id + " " + m_title +
+        " is now registered in the database");
+    }
+    catch(Exception ex) {
       out.println("\n Error: " + ex.getMessage());
-    } finally {
+    }
+    finally {
       out.close(); // Close stream
     }
   }
@@ -51,46 +53,47 @@ public class movie_reg extends HttpServlet {
   private void initializeJdbc() {
     try {
       // Declare driver and connection string
-      // String driver = "sun.jdbc.odbc.JdbcOdbcDriver";
-      // String connectionString = "jdbc:odbc:exampleMDBDataSource";
+     // String driver = "sun.jdbc.odbc.JdbcOdbcDriver";
+     // String connectionString = "jdbc:odbc:exampleMDBDataSource";
       // For MySQL
       // String driver = "com.mysql.jdbc.Driver";
       // String connectionString = "jdbc:mysql://localhost/test";
       // For Oracle
-      String driver = "oracle.jdbc.driver.OracleDriver";
-
+       String driver = "oracle.jdbc.driver.OracleDriver";
+       
       // Load the driver
       Class.forName(driver);
 
-      String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-      String user = "c##project";
-      String password = "project";
+    String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl"; //the oracle driver's url - the source locator. port 1521. orcl is the default database name, so you will need to change that if you named it differently.
+    String user = "project"; //this is the username for your database user 
+    String password = "project"; //this is the password for your database user 
 
-      // Connect to the database
-      Connection conn = DriverManager.getConnection(url, user, password);
+      // Connect to the sample database
+      Connection conn = DriverManager.getConnection //the method getConnection() is defined within sql.
+        (url, user, password);
 
-      // Create a Statement
+      // Create a Statement - this is where the sql code is
       pstmt = conn.prepareStatement("insert into movie " +
-          "(m_id, m_title, m_releaseDate, m_synopsis, m_length, m_rating, "
-          + "cat_id, productionStatus) values (?, ?, ? , ?, ?, ?, ?, ?)");
-    } catch (Exception ex) {
+        "(m_id, m_title, m_date, m_synopsis, m_length, rating_id, "
+         + "cat_id) values (?, ?, ?, ?, ?, ?, ?)");
+    }
+    catch (Exception ex) {
       ex.printStackTrace();
     }
   }
 
-  /** Store a record to the database */
-  private void storeMovie(String m_id, String m_title,
-      String m_releaseDate, String m_synopsis, String m_length, String m_rating,
-      String cat_id, String productionStatus) throws SQLException {
-    pstmt.setString(1, m_id);
+  /** Store a student record to the database */
+  private void storeStudent(String m_id, String m_title,
+      String m_date, String m_synopsis, String m_length, String rating_id,
+      String cat_id) throws SQLException {
+    pstmt.setString(1, m_id); 
     pstmt.setString(2, m_title);
-    pstmt.setDate(3, java.sql.Date.valueOf(m_releaseDate));
+    pstmt.setString(3, m_date);
     pstmt.setString(4, m_synopsis);
     pstmt.setString(5, m_length);
-    pstmt.setString(6, m_rating);
+    pstmt.setString(6, rating_id);
     pstmt.setString(7, cat_id);
-    pstmt.setString(8, productionStatus);
-
+    
     pstmt.executeUpdate();
   }
 }
