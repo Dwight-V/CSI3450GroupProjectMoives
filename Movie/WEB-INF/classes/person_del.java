42fd17e2d4a610a621f2a4ab6846f191c26623fc
JCSI2022 <113455662+LF92R@users.noreply.github.com>
2024-04-10 02:37:33 -0400

    Update person_del.java
    Currently a mess of if-else, but works. Will concise this down later.

--- Movie/WEB-INF/classes/person_del.java
+++ Movie/WEB-INF/classes/person_del.java
@@ -3,82 +3,201 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.sql.*;
 
+
 public class person_del extends HttpServlet {
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		Statement state4 = null;
-		ResultSet result = null;
-		String query = "";
-		Connection con = null;
+    
+    private Connection con; 
+    private PreparedStatement prepStatement; 
+    private Statement statement;
+    private PrintWriter out;
 
-		String m_id = request.getParameter("m_id");
-		String m_title = request.getParameter("m_title");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        //Get parameters from person_reg.html
+        String p_ID = request.getParameter("p_ID");
+        String role_name = request.getParameter("role_name");
+        String role_ID = request.getParameter("role_ID");
 
-		try {
-			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "project", "project");
-			System.out.println("Congratulations! You are connected successfully.");
-		} catch (SQLException e) {
-			System.out.println("Error: " + e);
-		} catch (Exception e) {
-			System.err.println("Exception while loading  driver");
-		}
-		try {
-			state4 = con.createStatement();
-		} catch (SQLException e) {
-			System.err.println("SQLException while creating statement");
-		}
+        try {
+            //Load driver for Oracle, and connect to the Oracle database
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "C##project", "project");
+            
+            String roleAttributeName_ID = "";
+            
+            //Delete from a selected role table
+            if (!role_ID.isEmpty() && p_ID.isEmpty()) { 
 
-		response.setContentType("text/html");
-		PrintWriter out = null;
-		try {
-			out = response.getWriter();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+                switch(role_name) {
+                    case "Producer":
+                        roleAttributeName_ID = "prod_ID";    
+                        break;
+                    case "Actor":
+                        roleAttributeName_ID = "actor_ID";
+                        break;
+                    case "Actress":
+                        roleAttributeName_ID = "actress_ID";
+                        break;
+                    case "Writer":
+                        roleAttributeName_ID = "writ_ID";
+                        break;
+                    case "Director":
+                        roleAttributeName_ID = "dir_ID";
+                        break;
+                    default:
+                        out.println("\n Error in passing attribute role_name.");
+                }
 
-		query = "delete  from movie where m_id = '" + m_id + "' or m_title = '" + m_title + "'";
+                //Checks if role table select is empty or not. 
+                if (checkIfRoleTableEmpty(role_name)) {
+                    //The role table select is empty - output message stating so
+                    response.setContentType("text/html");
+                    out = response.getWriter();
+                    out.println("<html><head><title>Empty table</title></head><body>");
+                    out.println("<br /><b><center><font color=\"RED\"><H2>Role table " + role_name + " IS empty.</H2></font></center><br />");
+                    out.println("</body></html>");
+                } else {
+                    //The role table selected is NOT empty - delete the entry with the entered role ID
+                    deleteRoleTableEntry(role_name, roleAttributeName_ID, role_ID);
+                    
+                    response.setContentType("text/html");
+                    out = response.getWriter();
+                    out.println("<html><head><title>Empty table</title></head><body>");
+                    out.println("<br /><b><center><font color=\"RED\"><H2>Role table " + role_name + " entry with role_ID: " + role_ID + " has been removed.</H2></font></center><br />");
+                    out.println("</body></html>");
+                }
 
-		out.println("<html><head><title>Movie has been deleted</title>");
-		out.println("</head><body>");
+            } else if(!(p_ID.isEmpty()) && role_ID.isEmpty()) { //delete a person and all of their role table entries
+                
+                boolean checkPersonTable = checkIfPersonTableEmpty();
+                boolean checkIfPersonExists = checkIfPersonExists(p_ID);
 
-		out.print("<br /><b><center><font color=\"BLACK\"><H2>One Record has deleted</H2></font>");
-		out.println("</center><br />");
-		try {
-			result = state4.executeQuery(query);
+                if (!checkPersonTable) {
+                    
+                    if (checkIfPersonExists) {
+                        // Perform actions when person doesn't exist
+                        response.setContentType("text/html");
+                        out = response.getWriter();
+                        out.println("<html><head><title>Error</title></head><body>");
+                        out.println("<br /><b><center><font color=\"RED\"><H2>No such person exists.</H2></font></center><br />");
+                        out.println("</body></html>");
+                    } else {
+                        deleteAllRoleTableEntries(p_ID); //To cycle through all role tables, and delete any entry that has the entered p_ID
+                        deleteFromPersonTable(p_ID); //To delete the person with such p_ID
+                        //Output if person entry has been succesfully deleted
+                        response.setContentType("text/html");
+                        out = response.getWriter();
+                        out.println("<html><head><title>Person has been deleted</title></head><body>");
+                        out.println("<br /><b><center><font color=\"BLACK\"><H2>The Person entry has been deleted.</H2></font></center><br />");
+                        out.println("</body></html>");
+                    }
+                   
+                } else if (checkPersonTable) {
+                    response.setContentType("text/html");
+                    out = response.getWriter();
+                    out.println("<html><head><title>Person table is empty</title></head><body>");
+                    out.println("<br /><b><center><font color=\"BLACK\"><H2>The Person table is empty.</H2></font></center><br />");
+                    out.println("</body></html>");
+                } 
+                
+            } else if (!p_ID.isEmpty() && !role_ID.isEmpty()) {
+                response.setContentType("text/html");
+                out = response.getWriter();
+                out.println("<html><head><title>Person table is empty</title></head><body>");
+                out.println("<br /><b><center><font color=\"BLACK\"><H2>Please choose to only delete a role entry or all of a person's entries.</H2></font></center><br />");
+                out.println("</body></html>");
+            } else {
+                response.setContentType("text/html");
+                out = response.getWriter();
+                out.println("<html><head><title>Person table is empty</title></head><body>");
+                out.println("<br /><b><center><font color=\"BLACK\"><H2>Please enter a person ID or role ID and role type value.</H2></font></center><br />");
+                out.println("</body></html>");
+            }
 
-		} catch (SQLException e) {
-			System.err.println("SQLException while executing SQL Statement.");
-		}
-		out.println("<center><table border=\"1\">");
-		out.println("<tr BGCOLOR=\"#cccccc\">");
-		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\"> </td>");
-		// out.println("<td align = \"justify\"><font face =\"times new roman\"
-		// size=\"4pt\">movtitle</td>");
-		out.println("</tr>");
-		try {
-			while (result.next()) {
-				out.println("<tr>");
-				out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"
-						+ result.getString(1) + "</td>");
-				// out.println("<td align = \"justify\"><font face =\"times new roman\"
-				// size=\"4pt\">"+result.getString(2)+"</td>");
-				out.println("</tr>");
-			}
-		} catch (SQLException e) {
-			System.out.println("Resutset is not connected");
-		}
+        } catch (SQLException e) {
+            //Output if person entry fails to delete
+            e.printStackTrace();
+            System.err.println("SQL Exception Message: " + e.getMessage()); 
+        } finally {
+            //Close connection
+            try {
+                if (con != null) con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-		out.println("</table></CENTER>");
-		try {
-			result.close();
-			state4.close();
-			con.close();
-			System.out.println("Connection is closed successfully.");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+    //Deletes role table entry
+    private void deleteRoleTableEntry(String role_name, String roleTableID, String p_ID) throws SQLException {
+        statement = con.createStatement();
+        statement.executeUpdate("DELETE FROM " + role_name + " WHERE " + roleTableID + " = " + p_ID);
+    }
+    
+    //Return true if the role table is empty, otherwise false.
+    private boolean checkIfRoleTableEmpty(String roleTableName) throws SQLException {
+        //For case where the role table is empty.
+        String query = "select count(*) from " + roleTableName;
+        prepStatement = con.prepareStatement(query);
+        ResultSet countResult = prepStatement.executeQuery();
+    
+        countResult.next();
+        int rowCount = countResult.getInt(1);
+    
+        // If the selected role table is empty, return true. If not, state the table is not empty.
+        return rowCount == 0;
+    }
+    
+    //Deletes the role table entries should the person entry with the p_ID be deleted - a safeguard.
+    private void deleteAllRoleTableEntries(String p_ID) throws SQLException {
+        String[] roleTables = {"actor", "actress", "director", "writer", "producer"};
+        String query;
+        for (String tableName : roleTables) {
+            //Cycle through all role tables, then delete any entries that have their p_ID equal to what the user inserted
+            query = "DELETE FROM " + tableName + " WHERE p_ID = ?";
+            prepStatement = con.prepareStatement(query);
+            prepStatement.setString(1, p_ID);
+            prepStatement.executeUpdate();
+        }
+    }
 
-		out.println("</body></html>");
-	}
+    //Checks if the person table is empty
+    private boolean checkIfPersonTableEmpty() throws SQLException {
+        String query = "select count(*) from person";
+        prepStatement = con.prepareStatement(query);
+        ResultSet countResult = prepStatement.executeQuery();
+
+        countResult.next();
+        int rowCount = countResult.getInt(1);
+
+        //If the person is empty, return true. If not, state the table is not empty.
+        if (rowCount == 0) {
+            return true;
+        } else {
+            return false;
+        }	
+    }
+
+    //Checks if the person entry with the p_ID exists or not
+    private boolean checkIfPersonExists(String p_ID) throws SQLException {
+        String query = "SELECT COUNT(*) FROM person WHERE p_ID = ?";
+        try (PreparedStatement prepStatement = con.prepareStatement(query)) {
+            prepStatement.setString(1, p_ID);
+            ResultSet resultSet = prepStatement.executeQuery();
+            resultSet.next(); // Move cursor to first row
+            if (resultSet.getInt(1) > 0 ) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    //Delete person entry with the p_ID entered - executed AFTER all role table entries with said p_ID are deleted.
+    private void deleteFromPersonTable(String p_ID) throws SQLException {
+        String query = "DELETE FROM person WHERE p_ID = ?";
+        prepStatement = con.prepareStatement(query);
+        prepStatement.setString(1, p_ID);
+        prepStatement.executeUpdate();
+    }
 }
