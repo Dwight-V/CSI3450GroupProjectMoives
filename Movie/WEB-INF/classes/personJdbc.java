dfb35afd5e1d93fcfb8800a2cfa4129ccf1dc804
JCSI2022 <113455662+LF92R@users.noreply.github.com>
2024-04-12 22:43:38 -0400

    Update personJdbc.java
    Works now.

--- Movie/WEB-INF/classes/personJdbc.java
+++ Movie/WEB-INF/classes/personJdbc.java
@@ -3,20 +3,24 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.sql.*;
 
-public class movieJdbc extends HttpServlet 
+public class personJdbc extends HttpServlet 
 {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException,IOException
     {        
+		
 			Statement state4 = null;
 			ResultSet result = null;
 			String query="";        
 			Connection con=null; 
 
 		try
-		{			
+		{
+			//Loads the driver for Oracle. 	
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver()); 
-            con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "project", "project");
+
+			//Connects to the database
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "C##CSI3450", "johnRing19841");
 	       	System.out.println("Congratulations! You are connected successfully.");      
      	}
         catch(SQLException e)
@@ -25,10 +29,12 @@ public class movieJdbc extends HttpServlet
 		}
 		catch(Exception e) 
 		{
-			System.err.println("Exception while loading  driver");		
+			System.err.println("Exception while loading driver");		
 		}
+
 	    try 
 		{
+			//Creates statement for executing SQL statements using Connection object con
         	state4 = con.createStatement();
 		} 
 		catch (SQLException e) 	
@@ -36,42 +42,50 @@ public class movieJdbc extends HttpServlet
 			System.err.println("SQLException while creating statement");			
 		}
 		
+		//Set content type of response 
 		response.setContentType("text/html");
-		PrintWriter out = null ;
+		PrintWriter out = null;
+
 		try
 		{
-			out =  response.getWriter();
+			//Gets HTTP responses, the entered information from the user into the HTTP fields
+			out = response.getWriter();
 		}
 		catch (IOException e) 
 		{
   			e.printStackTrace();
 		}
 		
-		query = "select m_title, m_date, m_length, cat_name, rating_name from movie, category, rating where movie.rating_id = rating.rating_id AND movie.cat_id = category.cat_id";
+		//Queries the database for the Person table's entries
+		query = "select p_id, p_lastName, p_firstName, p_pay from person";
 		
-		out.println("<html><head><title>Moive Table Report</title>");	 
+		out.println("<html><head><title>Person Table Report</title>");	 
 		out.println("</head><body>");
 		
-		out.print( "<br /><b><center><font color=\"RED\"><H2>Movie Table Report</H2></font>");
+		out.print( "<br /><b><center><font color=\"RED\"><H2>Person Table Report</H2></font>");
         out.println( "</center><br />" );
        	try 
 		{ 
-			result=state4.executeQuery(query);
+			//Executes SQL query to show all of the Person table's entries
+			result = state4.executeQuery(query);
 	  	}
 		catch (SQLException e) 
 		{
 			System.err.println("SQLException while executing SQL Statement."); 
 		}
+
+		//Setup for the table to be presented. Movie table attribute names are the names of the report table's columns.
 		out.println("<center><table border=\"1\">"); 
 		out.println("<tr BGCOLOR=\"#cccccc\">");
-        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Title</td>");
-        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Release Date</td>");
-		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Duration</td>");
-        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Category</td>");
-		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Rating</td>");
+        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Person ID</td>");
+		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Last Name</td>");
+        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">First Name</td>");
+		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Pay</td>");
         out.println("</tr>");
+		
 		try 
 		{ 
+			//Loop through the result set, and print each row
             while(result.next()) 
 			{ 
 		    	out.println("<tr>");
@@ -79,7 +93,6 @@ public class movieJdbc extends HttpServlet
 		    	out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(2)+"</td>");
 				out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(3)+"</td>");
 				out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(4)+"</td>");
-				out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(5)+"</td>");
                 out.println("</tr>");
 			} 
 	    }
@@ -91,6 +104,7 @@ public class movieJdbc extends HttpServlet
 		out.println("</table></CENTER>");
 		try 
 		{ 
+			//Close the result set, statement, and connection. Releases the resources required to connect to the database.
    			result.close(); 
 			state4.close(); 	
 			con.close();
